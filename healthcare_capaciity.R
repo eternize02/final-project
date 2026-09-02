f689da5f4992b64b2f8cf9c475a62f0d0c16fb4d
eileen ternize <eternize02@gmail.com>
2026-09-02 16:40:36 -0400

    finalized

---
+++ healthcare_capaciity.R
@@ -0,0 +1,115 @@
+
+{r}
+dat <- read_csv(here::here("data/raw/data.csv"))
+
+{r}
+#| warning: false
+#| message: false
+library(here)
+library(tidyverse)
+library(gtsummary)
+library(dplyr)
+library(here)
+
+mmsa_icu_beds <- read_csv(here::here( "mmsa-icu-beds.csv"))
+ny_mmsas <- mmsa_icu_beds %>%
+  filter(str_detect(MMSA, "NY"))
+
+{r}
+#| echo: false
+#| label: tbl-one
+#| tbl-cap: "Healthcare Capacity across metropolitain cities in New York"
+tbl_summary(ny_mmsas,
+            by = MMSA,
+            include = c(hospitals,high_risk_per_hospital, total_at_risk),
+            missing = "no",
+            type = list(hospitals ~ "continuous",
+                        high_risk_per_hospital ~ "continuous",
+                        total_at_risk ~ "continuous"),
+            statistic = all_continuous() ~ "{mean}",
+            label = list(high_risk_per_hospital ~ "High risk individuals per hospital",
+                         hospitals ~ "Hospitals in area",
+                         total_at_risk ~ "Total number of high risk individuals")) |>
+  add_overall(col_label = "**Mean**") |>
+  bold_labels() |>
+  remove_footnote_header() |>
+  modify_header(label = "**Variable**") |>
+  modify_caption("Healthcare Capacity across metropolitain cities in New York") 
+
+table1 <- tbl_summary(ny_mmsas,
+                      by = MMSA,
+                      include = c(hospitals,high_risk_per_hospital, total_at_risk),
+                      missing = "no",
+                      type = list(hospitals ~ "continuous",
+                                  high_risk_per_hospital ~ "continuous",
+                                  total_at_risk ~ "continuous"),
+                      statistic = all_continuous() ~ "{mean}",
+                      label = list(high_risk_per_hospital ~ "High risk individuals per hospital",
+                                   hospitals ~ "Hospitals in area",
+                                   total_at_risk ~ "Total number of high risk individuals")) |>
+  add_overall(col_label = "**Mean**") |>
+  bold_labels() |>
+  remove_footnote_header() |>
+  modify_header(label = "**Variable**") |>
+  modify_caption("Healthcare Capacity across metropolitain cities in New York") 
+
+{r}
+#| include: false
+table1$table_body
+
+
+{r}
+#| label: tbl-two
+#| tbl-cap: "Regression Analysis"
+linear <- lm(hospitals ~  total_at_risk, data = ny_mmsas)
+tbl_regression(linear,intercept = TRUE,
+               label = list(total_at_risk ~ "Total high risk individuals"))
+
+{r}
+#| label: fig-plot 
+#| fig-cap: "Scatterplot represents total number of high risk individuals in relation to the number of hospitals found across metropolitain areas in New York state."
+library(ggplot2) 
+library(tidyverse) 
+ggplot(mmsa_icu_beds, aes(x = total_at_risk, y = hospitals)) +   
+  geom_point(na.rm = TRUE, color = "royalblue3") +   
+  labs(     title = "Hospitals vs. Total High-Risk Individuals",     
+            x = "Total high-risk individuals",     
+            y = "Number of hospitals")
+
+{r}
+#| include: false
+#making new variable to then apply my function to find ratio per 100,000 people#
+ny_mmsas$hospitals_per_risk_individual <- (ny_mmsas$hospitals/ ny_mmsas$total_at_risk)
+
+hospital_ratio <- function (x)
+{n <- (x*100000)
+result <- (n)
+return(result)
+}
+x<-ny_mmsas$hospitals_per_risk_individual
+hospital_ratio(x)
+ny_mmsas$ratio <- hospital_ratio(x)
+
+#view and compare to verify function is correct
+ny_mmsas$hospitals_per_100k_at_risk <- 
+  (ny_mmsas$hospitals / ny_mmsas$total_at_risk) * 100000
+view (ny_mmsas[, c("hospitals_per_100k_at_risk", "ratio")])
+
+{r}
+#| label: fig-plot2
+#| fig-cap: "Bar chart represents hospital ratio for every 100k high risk individual."
+
+ggplot(ny_mmsas, aes(x =MMSA, y = ratio)) +
+  geom_bar(stat = "identity", fill = "royalblue1") +
+  labs(title = "Hospital Ratio for every 100k high risk individuals", x = "Metropolitain Area", y = "Ratio") +
+  theme_minimal()
+
+{r}
+##making statustic table
+#| include: false
+ny_ratio_table <- (ny_mmsas[, c("MMSA", "ratio")])
+view(ny_ratio_table)
+statistic <-list(
+  mean=round(mean(ny_ratio_table$ratio), digits = 2),
+  max=round(max(ny_ratio_table$ratio), digits = 2))
+
