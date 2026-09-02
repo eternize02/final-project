01e1ac516097a8341c736a0bad360942dc5dea9e
eileen ternize <eternize02@gmail.com>
2026-09-01 20:15:25 -0400

    finalized

--- README-finalproj.md
+++ README-finalproj.md
@@ -25,3 +25,6 @@ use e-cigarettes or vaping products every day or some days; or if they’re curr
 of these conditions but counted them only once each, so anyone with multiple conditions doesn’t get counted multiple times. We were not able to 
 include a number of conditions for which we did not have location-based data from the BRFSS, such as liver disease, having smoked, vaped or dabbed 
 marijuana in the last 30 days, and getting cancer treatment or being on immunosuppression medications.
+
+AI Usage: Conv ersation has been uploaded to github repository
+
--- healthcare_capaciity.R
+++ healthcare_capaciity.R
@@ -1,10 +1,3 @@
-
-{r}
-dat <- read_csv(here::here("data/raw/data.csv"))
-
-{r}
-#| warning: false
-#| message: false
 library(here)
 library(tidyverse)
 library(gtsummary)
@@ -15,10 +8,6 @@ mmsa_icu_beds <- read_csv(here::here( "mmsa-icu-beds.csv"))
 ny_mmsas <- mmsa_icu_beds %>%
   filter(str_detect(MMSA, "NY"))
 
-{r}
-#| echo: false
-#| label: tbl-one
-#| tbl-cap: "Healthcare Capacity across metropolitain cities in New York"
 tbl_summary(ny_mmsas,
             by = MMSA,
             include = c(hospitals,high_risk_per_hospital, total_at_risk),
@@ -53,21 +42,12 @@ table1 <- tbl_summary(ny_mmsas,
   modify_header(label = "**Variable**") |>
   modify_caption("Healthcare Capacity across metropolitain cities in New York") 
 
-{r}
-#| include: false
 table1$table_body
 
-
-{r}
-#| label: tbl-two
-#| tbl-cap: "Regression Analysis"
 linear <- lm(hospitals ~  total_at_risk, data = ny_mmsas)
 tbl_regression(linear,intercept = TRUE,
                label = list(total_at_risk ~ "Total high risk individuals"))
 
-{r}
-#| label: fig-plot 
-#| fig-cap: "Scatterplot represents total number of high risk individuals in relation to the number of hospitals found across metropolitain areas in New York state."
 library(ggplot2) 
 library(tidyverse) 
 ggplot(mmsa_icu_beds, aes(x = total_at_risk, y = hospitals)) +   
@@ -76,8 +56,6 @@ ggplot(mmsa_icu_beds, aes(x = total_at_risk, y = hospitals)) +
             x = "Total high-risk individuals",     
             y = "Number of hospitals")
 
-{r}
-#| include: false
 #making new variable to then apply my function to find ratio per 100,000 people#
 ny_mmsas$hospitals_per_risk_individual <- (ny_mmsas$hospitals/ ny_mmsas$total_at_risk)
 
@@ -95,18 +73,14 @@ ny_mmsas$hospitals_per_100k_at_risk <-
   (ny_mmsas$hospitals / ny_mmsas$total_at_risk) * 100000
 view (ny_mmsas[, c("hospitals_per_100k_at_risk", "ratio")])
 
-{r}
-#| label: fig-plot2
-#| fig-cap: "Bar chart represents hospital ratio for every 100k high risk individual."
 
 ggplot(ny_mmsas, aes(x =MMSA, y = ratio)) +
   geom_bar(stat = "identity", fill = "royalblue1") +
   labs(title = "Hospital Ratio for every 100k high risk individuals", x = "Metropolitain Area", y = "Ratio") +
   theme_minimal()
 
-{r}
+
 ##making statustic table
-#| include: false
 ny_ratio_table <- (ny_mmsas[, c("MMSA", "ratio")])
 view(ny_ratio_table)
 statistic <-list(
